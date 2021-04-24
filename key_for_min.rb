d0b19fb33ffad49bd9bdd42486ea6d9581a6276d
Sarah <sarahlchavez@gmail.com>
2021-04-24 06:07:17 -0700

    commit tree fa9ad507e2934819b9ae5bba58053a3e1ad9f8b4

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_key = nil
+    min_value = Float::INFINITY  # I got the idea to use Float::Infinity from: https://github.com/learn-co-curriculum/key-for-min-value/issues/9but I wanted to set the min_value to the first iteration 
+    name_hash.each do |key, value|
+        if value < min_value
+            min_value = value
+            min_key = key
+        end
+    end
+    min_key
 end
